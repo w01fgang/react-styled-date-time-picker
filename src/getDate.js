db6cff53e81b494ccc84455dec883b23ca7d8a56
Petar Josifovski <petar.pupuc@gmail.com>
2019-06-17 15:31:58 +0200

    fix
    fixed bug when chossing 31 dec. 2018...

--- src/getDate.js
+++ src/getDate.js
@@ -1,5 +1,5 @@
 export default (i, w, value) => {
-  const nextMonth = (w > 4);
+  const nextMonth = (w > 5);
   let date = value;
   if (nextMonth) date = date.plus({ month: 1 });
   date = date.set({ day: i });
